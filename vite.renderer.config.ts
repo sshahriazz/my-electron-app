72535ee61d2ea193d6d41d8b811e31d1c0ef3b75
shiam-technext <141896624+shiam-technext@users.noreply.github.com>
2025-01-09 00:04:23 +0600

    Revert "Merge branch 'main' into dev/user-activity"
    This reverts commit 74474926721527ffd139c548f172bfc7aaf2ce89, reversing
    changes made to b5533250316a09d21ac58665bd7acdd24396a75b.

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  base: './',
-  build: {
-    outDir: '.vite/renderer',
-    emptyOutDir: true,
-  },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+  }
 });
